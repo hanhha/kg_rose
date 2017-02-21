9487f4b7999487a8195cf4d48089e8dcf298b229
Hanh Ha <tranhanh.haminh@gmail.com>
2017-02-21 16:04:01 +0700

    Result, visualization
    Add results and visualization

--- review.py
+++ review.py
@@ -1,6 +1,8 @@
 from collections import Counter as cnt
 from ggplot import *
 import matplotlib.pyplot as plt
+import pylab
+from mpl_toolkits.mplot3d import Axes3D
 import pandas as pd 
 import numpy as np
 import re
@@ -13,12 +15,32 @@ from sklearn.metrics import classification_report,confusion_matrix
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
 
+def visualize_data(X, Y):
+  # Compute PCA
+  print "Using PCA to reduce to 3D dataset"
+  sigma = X.cov() # Compute covariance matrix of preprocessed data
+  U, s, V = np.linalg.svd(sigma, full_matrices=True, compute_uv=True)
+  Ureduce = U [:, range(0,3)]
+  pa_train_set = X.dot(Ureduce).rename(columns={0:'P0',1:'P1',2:'P2'})
+  variance_retained = s[range(0,3)].sum() / s.sum()
+  print "Variance retained: %f" % variance_retained
+  m_lbl_survived = Y['Survived'].apply(lambda e: 'Survived' if e == 1 else 'Dead')
+  data_plot = pa_train_set.assign(Survived=m_lbl_survived)
+  fig = pylab.figure()
+  ax = Axes3D(fig)
+  ax.scatter(data_plot['P0'], data_plot['P1'], data_plot['P2'])
+  plt.show()
+  #print data_plot
+  #p = ggplot(data_plot, aes(x='P0',
+  #y='P1',z='P2', color = 'factor(Survived)'))
+  #p = p + geom_point() + scale_color_manual(values=["red","green"]) + ggtitle("Survived")
+  #p.show()
+  
 #==================================
 print "Reading training data ..."
 #==================================
 train_set = pd.read_csv ('Data/train.csv', sep=',', header=0, 
 			skip_blank_lines=True, quotechar='"')
-M = train_set.shape [0] # Size of training set
 
 # Predict factors:
 #   - Sex (1 - Male)
@@ -67,19 +89,7 @@ s_train_set = (m_train_set - m_train_set.mean()) / (m_train_set.max() -
 #==================================
 print "Visulizing training data in 2D space"
 #==================================
-# Compute PCA
-print "Using PCA to reduce to 2D dataset"
-sigma = s_train_set.cov() # Compute covariance matrix of preprocessed data
-U, s, V = np.linalg.svd(sigma, full_matrices=True, compute_uv=True)
-Ureduce = U [:, range(0,2)]
-pa_train_set = s_train_set.dot(Ureduce).rename(columns={0:'P0',1:'P1'})
-variance_retained = s[range(0,2)].sum() / s.sum()
-print "Variance retained: %f" % variance_retained
-m_lbl_survived = m_survived.applymap(lambda e: 'Survived' if e == 1 else 'Dead')
-p = ggplot(pa_train_set.assign(Survived=m_lbl_survived['Survived']), aes(x='P0',
-y='P1', color = 'factor(Survived)'))
-p = p + geom_point() + scale_color_manual(values=["red","green"]) + ggtitle("Survived")
-print p
+visualize_data (s_train_set, m_survived)
 
 #==================================
 print "Split training set to training and test sets"
@@ -90,33 +100,97 @@ X = s_train_set[list(s_train_set.columns)].values
 #==================================
 print "Trying multiple classifiers:"
 #==================================
-names = ["Linear SVM", "RBF SVM", "Decision Tree","Random Forest",
+scores = [0] * 7
+names = ["KNN", "Linear SVM", "RBF SVM", "Decision Tree","Random Forest",
 		"Neural Network", "AdaBoost", "Naive Bayes"]
-classifiers = [SVC(kernel="linear", C=0.025),
+classifiers = pd.DataFrame([[1,2,3,4,5,6,7,8],
+              names,
+               [KNeighborsClassifier(11),
+                 SVC(kernel="linear", C=0.025),
 							 SVC(gamma=2, C=1),
 							 DecisionTreeClassifier(max_depth=6),
 							 RandomForestClassifier(max_depth=6, n_estimators=10,
 								 max_features=None),
 							 MLPClassifier(alpha=1),
 							 AdaBoostClassifier(),
-							 GaussianNB()]
+							 GaussianNB()],
+							 scores]).T.rename(columns={0:'Idx', 1:'Name', 2:'Clf', 3:'Score'})
 
 X_train, X_test, Y_train, Y_test = train_test_split (X, Y)
 
-scores = [0] * 7
-i = 0
-for c, clf in zip(names, classifiers):
-	print "%s" % c
-	clf.fit(X_train, Y_train)
-	scores [i] = clf.score(X_test, Y_test)
-	print "   score: %f" % scores[i]
-	i += 1
+for idx, clf in classifiers.iterrows():
+	print "%s" % clf['Name']
+	clf['Clf'].fit(X_train, Y_train)
+	clf['Score'] = clf['Clf'].score(X_test, Y_test)
+	print "   score: %f" % clf['Score']
 	
-classifiers_scores = pd.DataFrame([[1,2,3,4,5,6,7], names,
-	scores]).T.rename(columns={0:'Idx', 1:'Name',2:'Score'})
-plt.plot (classifiers_scores['Idx'], classifiers_scores['Score'], 'ro')
-plt.xticks(classifiers_scores['Idx'], classifiers_scores['Name'], rotation =
+plt.plot (classifiers['Idx'], classifiers['Score'], 'ro')
+plt.xticks(classifiers['Idx'], classifiers['Name'], rotation =
 		'vertical')
 plt.margins(0.2)
 plt.subplots_adjust(bottom=0.3)
 plt.show()
+
+#==================================
+print "Pick the most accuracy model ..."
+#==================================
+selected_clf      = classifiers.loc[classifiers['Score'].idxmax()]
+print "%s is selected" %selected_clf['Name']
+
+#==================================
+print "Reading test data ..."
+#==================================
+test_set = pd.read_csv ('Data/test.csv', sep=',', header=0, 
+			skip_blank_lines=True, quotechar='"')
+
+#==================================
+print "Extracting and numerizing features and then classifying test data ..."
+#==================================
+m_test_set = pd.DataFrame(columns=[
+			'Sex','Age', 'SibSp','Parch', 'Pclass',
+			'Floor'])
+m_pred_survived = pd.DataFrame(columns=['PassengerId', 'Survived'])
+
+for idx, row in test_set[['PassengerId','Sex','Age','SibSp',
+			'Parch','Pclass','Cabin']].iterrows():
+	cabins   = row['Cabin'].split(' ') if pd.notnull(row['Cabin']) else ['Z']
+	sex      = 1 if row['Sex'] == 'female' else 0
+	age      = row['Age'] if pd.notnull(row['Age']) else 0
+	sibsp    = row['SibSp']
+	parch    = row['Parch']
+	pclass   = row['Pclass']
+	pid      = row['PassengerId']
+
+	if len(cabins) > 1:
+	  tmp_predict = 0
+	  location = len(m_pred_survived)
+	  for i in cabins:
+			floor = ord(re.findall('[A-Z]',i)[0]) - 65
+			sample = pd.Series({'Pclass':pclass, 'Floor':floor, 'Sex':sex, 'Age':age, 
+								'SibSp':sibsp, 'Parch':parch})
+			# Normalizing and scaling data
+			sample = (sample - m_train_set.mean()) / (m_train_set.max() - m_train_set.min())
+			#print sample[['Sex','Age','SibSp','Parch','Pclass','Floor']].values
+			if (tmp_predict == 0):
+			  m_test_set.loc[location] = sample
+			  tmp_predict = selected_clf['Clf'].predict(sample[['Sex','Age','SibSp','Parch','Pclass','Floor']].values.reshape(1,-1))[0]
+			  m_pred_survived.loc[location]={'PassengerId':pid,'Survived':tmp_predict}
+	else:
+		floor = ord(re.findall('[A-Z]',cabins[0])[0]) - 65
+		sample = pd.Series({'Pclass':pclass, 'Floor':floor, 'Sex':sex, 'Age':age, 
+								'SibSp':sibsp, 'Parch':parch})
+		# Normalizing and scaling data
+		sample = (sample - m_train_set.mean()) / (m_train_set.max() - m_train_set.min())
+		m_test_set.loc[len(m_test_set)] = sample
+		#print sample[['Sex','Age','SibSp','Parch','Pclass','Floor']].values
+		m_pred_survived.loc[len(m_pred_survived)]={'PassengerId':pid,'Survived':selected_clf['Clf'].predict(sample[['Sex','Age','SibSp','Parch','Pclass','Floor']].values.reshape(1,-1))[0]}
+
+#==================================
+print "Visulizing classified test data in 2D space"
+#==================================
+visualize_data (m_test_set, m_pred_survived)
+
+#==================================
+print "Writing to submission file ..."
+#==================================
+m_pred_survived.fillna(0).astype(int).to_csv('Data/submission.csv', index=False)
